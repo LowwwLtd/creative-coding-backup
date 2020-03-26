0285c40eb48a99aa0e93292087491ef1ee7a8ff6
andrevenancio <noreply@andrevenancio.com>
2020-03-26 13:56:18 +0000

    refactor

--- src/server/gate.js
+++ src/server/gate.js
@@ -1,13 +1,10 @@
-/*
-https = false will always redirect to http
-*/
+/* eslint-disable no-nested-ternary */
 module.exports = {
     name: 'hapi-gate-rebuild',
     version: '1.0.0',
 
     register: (server, options) => {
-        server.ext('onRequest', function(request, h) {
-            // stores original request protocol and host
+        server.ext('onRequest', (request, h) => {
             const original = {
                 protocol:
                     (options.proxy !== false
@@ -17,64 +14,22 @@ module.exports = {
                     request.headers['x-forwarded-host'] || request.headers.host,
             };
 
-            let redirect = false;
-            let protocol = options.https ? 'https' : 'http';
-            let host = options.www
+            const protocol = options.https ? 'https' : 'http';
+            const host = options.www
                 ? /^www\./.test(original.host)
                     ? original.host
                     : `www.${original.host}`
                 : original.host.replace(/^www\./, '');
-            const pathname = request.url.pathname;
-            const search = request.url.search;
 
-            // compares original with refactored values and only does a redirect if necessary
             if (protocol !== original.protocol || host !== original.host) {
-                redirect = true;
+                const { pathname, search } = request.url;
+                return h
+                    .redirect(`${protocol}://${host}${pathname}${search}`)
+                    .takeover()
+                    .code(301);
             }
 
-            if (!redirect) return h.continue;
-
-            console.log(
-                'redirect',
-                `${protocol}://${host}${pathname}${search}`
-            );
-            return h
-                .redirect(`${protocol}://${host}${pathname}${search}`)
-                .takeover()
-                .code(301);
+            return h.continue;
         });
     },
 };
-
-/*
-const Url = require('url');
-
-module.exports = {
-    name: 'hapi-gate-rebuild',
-    version: '1.0.0',
-
-    register: (server, options) => {
-        server.ext('onRequest', function(request, h) {
-            const redirect =
-                options.proxy !== false
-                    ? request.headers['x-forwarded-proto'] === 'http'
-                    : request.server.info.protocol === 'http';
-            const host =
-                request.headers['x-forwarded-host'] || request.headers.host;
-
-            console.log('request', request);
-            if (!redirect) return h.continue;
-            return h
-                .redirect(
-                    'https://' +
-                        host +
-                        request.url.pathname +
-                        request.url.search
-                )
-                .takeover()
-                .code(301);
-        });
-    },
-};
-
-*/
