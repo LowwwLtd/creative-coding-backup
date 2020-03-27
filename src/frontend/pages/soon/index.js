7303a8712ea793511318c29d598d511a11b66ecf
andrevenancio <noreply@andrevenancio.com>
2020-03-27 00:22:09 +0000

    coming soon

---
+++ src/frontend/pages/soon/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Logo } from 'app/components';
+import './style.scss';
+
+export const SoonPage = () => {
+    return (
+        <div className="soon">
+            <Logo />
+            <p>coming soon</p>
+        </div>
+    );
+};
