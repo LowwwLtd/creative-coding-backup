7887f6f7da83fa421da770906f9f39db108ff848
andrevenancio <noreply@andrevenancio.com>
2020-03-27 00:23:31 +0000

    staging

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-// import { HomePage } from 'app/pages/home';
-// import { PostPage } from 'app/pages/post';
-import { SoonPage } from '../pages/soon';
+import { HomePage } from 'app/pages/home';
+import { PostPage } from 'app/pages/post';
 
 export const routes = [
     {
         path: '/',
         exact: true,
-        child: <SoonPage />,
+        child: <HomePage />,
     },
-    // {
-    //     path: '/post/:postId',
-    //     child: <PostPage />,
-    // },
+    {
+        path: '/post/:postId',
+        child: <PostPage />,
+    },
 ];
